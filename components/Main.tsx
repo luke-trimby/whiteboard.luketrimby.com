93b9d6b1f31883f0b0cf788123ac5efaaa0e10f8
Luke Trimby <luketrimby@outlook.com>
2023-07-01 16:50:52 +0100

    canvas and context ref

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useCanvas from '../hooks/useCanvas';
 
 export default function Main() {
 
-  const canvas = useCanvas({ width: 4096, height: 4096, className: "sticky top-12 left-0 bg-white" });
+  const { canvas } = useCanvas({ width: 1200, height: 1200, className: "sticky top-12 left-0 bg-white" });
 
   return (
     <section className="text-black body-font">
--- hooks/useCanvas.tsx
+++ hooks/useCanvas.tsx
@@ -8,19 +8,42 @@ interface CanvasProps {
 
 export default function Canvas(props: CanvasProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<CanvasRenderingContext2D>(null);
+
+  const drawGrid = (gridSize: number = 100) => {
+    for (let x = 0; x <= canvasRef.current.width; x += gridSize) {
+      for (let y = 0; y <= canvasRef.current.height; y += gridSize) {
+        contextRef.current.rect(x, y, gridSize, gridSize);
+      }
+    }
+    contextRef.current.strokeStyle = "#aaa";
+    contextRef.current.lineWidth = 0.5;
+    contextRef.current.stroke();
+  };
+
+  const drawDebugCenterCircle = () => {
+    contextRef.current.beginPath();
+    contextRef.current.fillStyle = 'red';
+    contextRef.current.arc(canvasRef.current.width / 2, canvasRef.current.height / 2, 50, 0, 2 * Math.PI);
+    contextRef.current.fill();
+  }
+
+  const initCanvasContent = () => {
+    drawGrid();
+    drawDebugCenterCircle();
+  }
 
   useEffect(() => {
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
-      if (context) {
-        context.beginPath();
-        context.fillStyle = 'red';
-        context.arc(100, 100, 100, 0, 2 * Math.PI);
-        context.fill();
+      contextRef.current = canvasRef.current.getContext('2d');
+      if (contextRef.current) {
+        initCanvasContent();
       }
     }
   }, []);
 
-  return <canvas ref={canvasRef} {...props} />;
+  return {
+    canvas: <canvas ref={canvasRef} {...props} />,
+    context: contextRef
+  };
 };
